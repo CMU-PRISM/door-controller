245f2b7e6775bb634effa4e2d110284b42644b5c
Casey Rodriguez <itscaseyr@gmail.com>
2022-07-20 04:01:39 -0400

    Shifted authentication to handle in view

--- doorbot.py
+++ doorbot.py
@@ -7,17 +7,16 @@ import RPi.GPIO as GPIO
 
 # Constants
 TWO_WEEKS_SECONDS = 1210000
+FIVE_MINUTES_SECONDS = 300
 BTN_PIN = 7
 OPN_PIN = 11
 BSY_PIN = 13
 CLS_PIN = 15
-LOGIN_URL = 'https://prism.andrew.cmu.edu/accounts/login'
 SITE_URL = 'https://prism.andrew.cmu.edu'
 OPEN_URL = 'https://prism.andrew.cmu.edu/door-open'
 BUSY_URL = 'https://prism.andrew.cmu.edu/door-busy'
 CLOSE_URL = 'https://prism.andrew.cmu.edu/door-close'
-USER_NAME = 'doorbot@prism.andrew.cmu.edu'
-PWD = 'PUT PASSWORD HERE DURING SETUP'
+PWD = 'SET PWD DURING SETUP'
 PRISM_CERTS = 'prismcert.pem'
 
 # Use physical pin numbering
@@ -31,64 +30,74 @@ GPIO.setup(CLS_PIN, GPIO.OUT)
 
 # Variables
 session_limit = 0
+door_limit = 0
 rqst = ''
 rsp = ''
 
-# Switch LEDs to OPN_PIN on
-def open_pin():
-    GPIO.output(OPN_PIN, GPIO.HIGH)
-    GPIO.output(BSY_PIN, GPIO.LOW)
-    GPIO.output(CLS_PIN, GPIO.LOW)
-
-# Switch LEDs to BSY_PIN on
-def busy_pin():
-    GPIO.output(OPN_PIN, GPIO.LOW)
-    GPIO.output(BSY_PIN, GPIO.HIGH)
-    GPIO.output(CLS_PIN, GPIO.LOW)
-
-# Switch LEDs to CLS_PIN on
-def close_pin():
+def change_pin(doorstate):
+    # Reset all pins
     GPIO.output(OPN_PIN, GPIO.LOW)
     GPIO.output(BSY_PIN, GPIO.LOW)
-    GPIO.output(CLS_PIN, GPIO.HIGH)
+    GPIO.output(CLS_PIN, GPIO.LOW)
+    # If doorstate starts with 'OPEN'
+    if doorstate[0] == 'O':
+        high_pin = OPN_PIN
+    # If doorstate starts with 'BUSY'
+    if doorstate[0] == 'B':
+        high_pin = BSY_PIN
+    # If doorstate starts with 'CLOSED'
+    if doorstate[0] == 'C':
+        high_pin = CLS_PIN
+    # Set pin corresponding with doorstate high
+    GPIO.output(high_pin, GPIO.HIGH)
 
 # Run forever
 while True:
-    # In two weeks time, restart session
-    session_limit = time.time() + TWO_WEEKS_SECONDS
-    # Load in new session data
-    rqst = requests.session()
-    rqst.headers.update({'referer': SITE_URL})
-    rsp = rqst.get(LOGIN_URL, verify=PRISM_CERTS)
-    token = rsp.cookies['csrftoken']
-    rsp = rqst.post(LOGIN_URL, verify=PRISM_CERTS,
-                    data = {'email': USER_NAME, 'password': PWD,
-                            'csrfmiddlewaretoken': token})
+    # If more than two weeks have passed since starting session, restart session
+    if time.time() > session_limit:
+        # Set limit to two weeks from now
+        session_limit = time.time() + TWO_WEEKS_SECONDS
+        # Load in new session data
+        rqst = requests.session()
+        rqst.headers.update({'referer': SITE_URL})
+        rsp = rqst.get(SITE_URL, verify=PRISM_CERTS)
+        token = rsp.cookies['csrftoken']
 
-    # Restart session after two weeks
-    while time.time() < session_limit:
-        # If the button is pressed, start switching the room status
-        if GPIO.input(BTN_PIN) == GPIO.HIGH:
-            # get current door state
-            r = rqst.get(SITE_URL, verify=PRISM_CERTS)
-            soup = BeautifulSoup(r.content, "html.parser")
-            oldState = soup.find(id="door-status").text
-            # If room is was open: mark busy
-            if oldState[0] == 'O':
-                rqst.post(OPEN_URL, verify=PRISM_CERTS,
-                    data = {'csrfmiddlewaretoken': token})
-                open_pin()
-            # If room is was busy: mark closed
-            elif oldState[0] == 'B':
-                rqst.post(BUSY_URL, verify=PRISM_CERTS,
-                    data = {'csrfmiddlewaretoken': token})
-                busy_pin()
-            # If room is was closed: mark open
-            elif oldState[0] == "C":
-                rqst.post(CLOSE_URL, verify=PRISM_CERTS,
-                    data = {'csrfmiddlewaretoken': token})
-                close_pin()
-            else:
-                print("ERROR: Room state unknown!")
-        # Pause before taking new input
-        time.sleep(0.25)
+    # If more than five minutes have passed since last check, get door state again
+    if time.time() > door_limit:
+        # Set limit to five minutes from now
+        door_limit = time.time() + FIVE_MINUTES_SECONDS
+        r = rqst.get(SITE_URL, verify=PRISM_CERTS)
+        soup = BeautifulSoup(r.content, "html.parser")
+        doorState = soup.find(id="door-status").text
+        change_pin(doorState)
+
+    # If the button is pressed, start switching the room status
+    if GPIO.input(BTN_PIN) == GPIO.HIGH:
+        # get current door state
+        r = rqst.get(SITE_URL, verify=PRISM_CERTS)
+        soup = BeautifulSoup(r.content, "html.parser")
+        oldState = soup.find(id="door-status").text
+        # If room is was open: mark busy
+        if oldState[0] == 'O':
+            rqst.post(BUSY_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('BUSY')
+        # If room is was busy: mark closed
+        elif oldState[0] == 'B':
+            rqst.post(CLOSE_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('CLOSED')
+        # If room is was closed: mark open
+        elif oldState[0] == "C":
+            rqst.post(OPEN_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('OPEN')
+        # Unknown, attempt to close door
+        else:
+            print("ERROR: Room state unknown!")
+            rqst.post(CLOSE_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('CLOSED')
+    # Pause before taking new input
+    time.sleep(0.25)
--- manual_control.py
+++ manual_control.py
@@ -2,52 +2,88 @@ from bs4 import BeautifulSoup
 import requests, time
 
 # Constants
+TWO_WEEKS_SECONDS = 1210000
+FIVE_MINUTES_SECONDS = 300
 LOGIN_URL = 'https://prism.andrew.cmu.edu/accounts/login'
 SITE_URL = 'https://prism.andrew.cmu.edu'
 OPEN_URL = 'https://prism.andrew.cmu.edu/door-open'
 BUSY_URL = 'https://prism.andrew.cmu.edu/door-busy'
 CLOSE_URL = 'https://prism.andrew.cmu.edu/door-close'
-USER_NAME = 'doorbot@prism.andrew.cmu.edu'
-PWD = 'PUT PASSWORD HERE DURING SETUP'
+PWD = 'SET PWD DURING SETUP'
 PRISM_CERTS = 'prismcert.pem'
 
 # Variables
 session_limit = 0
+door_limit = 0
 rqst = ''
 rsp = ''
 
+def change_pin(doorstate):
+    # Reset all pins
+    print("OPEN pin lowered")
+    print("BUSY pin lowered")
+    print("CLOSE pin lowered")
+    # If doorstate starts with 'OPEN'
+    if doorstate[0] == 'O':
+        high_pin = 'OPN_PIN'
+    # If doorstate starts with 'BUSY'
+    if doorstate[0] == 'B':
+        high_pin = 'BSY_PIN'
+    # If doorstate starts with 'CLOSED'
+    if doorstate[0] == 'C':
+        high_pin = 'CLS_PIN'
+    # Set pin corresponding with doorstate high
+    print("%s set to high" % high_pin)
+
 # Run forever
 while True:
-    session_limit = time.time() + 10000
-    rqst = requests.session()
-    rqst.headers.update({'referer': SITE_URL})
-    rsp = rqst.get(LOGIN_URL, verify=PRISM_CERTS)
-    token = rsp.cookies['csrftoken']
-    rsp = rqst.post(LOGIN_URL, verify=PRISM_CERTS,
-        data = {'email': USER_NAME, 'password': PWD,
-                'csrfmiddlewaretoken': token})
+    # If more than two weeks have passed since starting session, restart session
+    if time.time() > session_limit:
+        # Set limit to two weeks from now
+        session_limit = time.time() + TWO_WEEKS_SECONDS
+        # Create new session
+        rqst = requests.session()
+        rqst.headers.update({'referer': SITE_URL})
+        rsp = rqst.get(SITE_URL, verify=PRISM_CERTS)
+        token = rqst.cookies['csrftoken']
 
-    # Restart session after two weeks
-    while time.time() < session_limit:
+    # If more than five minutes have passed since last check, get door state again
+    if time.time() > door_limit:
+        # Set limit to five minutes from now
+        door_limit = time.time() + FIVE_MINUTES_SECONDS
+        r = rqst.get(SITE_URL, verify=PRISM_CERTS)
+        soup = BeautifulSoup(r.content, "html.parser")
+        doorState = soup.find(id="door-status").text
+        change_pin(doorState)
+
+    # If button pressed, change door state on site (always true for tests)
+    if input("Press button? True/False: ") == "True":
         # get current door state
         r = rqst.get(SITE_URL, verify=PRISM_CERTS)
         soup = BeautifulSoup(r.content, "html.parser")
-        # If button pressed, change door state on site
         oldState = soup.find(id="door-status").text
-        print(oldState)
-        newState = input("New door status: ")
-        if newState == "open":
-            rqst.post(OPEN_URL, verify=PRISM_CERTS,
-                data = {'csrfmiddlewaretoken': token})
-            print("Door opened!")
-        elif newState == "close":
-            rqst.post(CLOSE_URL, verify=PRISM_CERTS,
-                data = {'csrfmiddlewaretoken': token})
-            print("Door closed!")
-        elif newState == "busy":
+        print("Door was " + oldState)
+        # If room is was open: mark busy
+        if oldState[0] == "O":
             rqst.post(BUSY_URL, verify=PRISM_CERTS,
-                data = {'csrfmiddlewaretoken': token})
-            print("Room marked busy!")
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('BUSY')
+        # If room is was busy: mark closed
+        elif oldState[0] == "B":
+            rqst.post(CLOSE_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('CLOSED')
+        # If room is was closed: mark open
+        elif oldState[0] == "C":
+            rqst.post(OPEN_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('OPEN')
+        # Unknown, attempt to close door
         else:
-            print("INVALID INPUT. YOU SAID: ")
-            print(newState)
+            print("INVALID INPUT. INPUT WAS: ")
+            print(oldState)
+            rqst.post(CLOSE_URL, verify=PRISM_CERTS,
+                data = {'csrfmiddlewaretoken': token, 'password': PWD})
+            change_pin('CLOSED')
+    # Pause before taking new input
+    time.sleep(0.25)
